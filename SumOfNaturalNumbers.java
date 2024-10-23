867a6723c6912a59653545086348437ba8720e8b
Sumeetlohana <kumar2004sumeet@gmail.com>
2024-10-22 22:01:26 -0700

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(n * (n + 1) / 2);
+    }
+}
